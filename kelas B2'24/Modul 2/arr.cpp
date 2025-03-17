eb26b1c0f5c93905b815e743d5003c80049175de
riyadiambis <rahmatriyadiambis@gmail.com>
2025-03-17 14:34:20 +0800

    pertama

---
+++ kelas B2'24/Modul 2/arr.cpp
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+#include<iostream>
+using namespace std;
+
+int main(){
+    string nama[5] = {"riyadi", "kiki", "emi", "ridwan", "baco"};
+    for(int i = 0; i < 5; i++){
+        cout << nama[i] << endl;  // Mengubah nama[0] menjadi nama[i]
+    }
+    return 0;  // Menambahkan return statement
+=======
+#include<iostream>
+using namespace std;
+
+int main(){
+    string nama[5] = {"riyadi", "kiki", "emi", "ridwan", "baco"};
+    for(int i = 0; i < 5; i++){
+        cout << nama[i] << endl;  // Mengubah nama[0] menjadi nama[i]
+    }
+    return 0;  // Menambahkan return statement
+>>>>>>> ec2900c (tambah kelas)
+}
